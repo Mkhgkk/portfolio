e6e188757b16a2f034c0903d965ac31f731f8329
Mkhgkk <emmachalz@icloud.com>
2021-09-29 17:16:22 +0800

    🎗 preventing opening website from mobile

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  document.body.innerHTML =
+    "Please visit this site on Desktop. I have not figured out how to display some wider elements on mobile yet.";
+}
+
 function navigate(url) {
   window.location.href = url;
 }
